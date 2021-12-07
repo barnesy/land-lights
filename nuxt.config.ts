788e6ed880ccdb51874d26ad7a7823a875503027
Ian Purvis <ian@purvisresearch.com>
2021-12-06 18:25:08 -0600

    Eliminate gas-digital dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: [
-    '@gas-digital/components/lib/assets/styles/global.scss',
     '~/assets/styles/global.scss'
   ],
   srcDir: 'src/'
--- src/app.vue
+++ src/app.vue
@@ -1,84 +1,72 @@
 <template>
-  <PageSection
-    class="dark"
-    bgColor="var(--red)"
-    :bgImage="`url('${bgImageURL}')`"
-  >
-    <Header class="v2">
-      <template>
-        HeartbeatATL
-
-        <img v-on:click="askForLocation" class="heart" src="~/assets/img/heart.png" width="150" height="150" />
-      </template>
-      <template #support>
-        {{location.lat + "  " + location.lon}}
-      </template>
-
-    </Header>
-
-  </PageSection>
+  <section class="section">
+    <h1>HeartbeatATL</h1>
+    <img @click="askForLocation" class="heart" src="~/assets/img/heart.png"/>
+    <div>
+      {{location.lat + "  " + location.lon}}
+    </div>
+  </section>
 </template>
 
 <script>
-import Header from '@gas-digital/components/lib/Header.vue';
-import PageSection from '@gas-digital/components/lib/PageSection.vue';
-import bgImageURL from '~/assets/img/bg.jpg';
-
-  export default {
-    name: "Index",
-    components: {
-      Header,
-      PageSection
-    },
-    data() {
-      return {
-        bgImageURL,
-        location: {
-          lat: null,
-          lon: null
-        }
-      }
-    },
-    methods : {
-      askForLocation() {
-        var startPos;
-        console.log(this.location.lat);
-        var t = this;
-
-        var geoSuccess = function(position) {
-          startPos = position;
-          t.location.lat = startPos.coords.latitude;
-          t.location.lon = startPos.coords.longitude;
-        };
-
-        var geoError = function(error) {
-          switch(error.code) {
-            case error.TIMEOUT:
-              console.log(error);
-              break;
-          }
-        };
-
-        navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+export default {
+  data() {
+    return {
+      location: {
+        lat: null,
+        lon: null
       }
     }
+  },
+  methods : {
+    askForLocation() {
+      var startPos;
+      console.log(this.location.lat);
+      var t = this;
+
+      var geoSuccess = function(position) {
+        startPos = position;
+        t.location.lat = startPos.coords.latitude;
+        t.location.lon = startPos.coords.longitude;
+      };
+
+      var geoError = function(error) {
+        switch(error.code) {
+          case error.TIMEOUT:
+            console.log(error);
+            break;
+        }
+      };
+
+      navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+    }
+  }
 };
 
 </script>
 
 <style lang="scss">
-.page-section {
-  min-height: 90rem;
-}
 
-.v2 .header .support {
-  text-align: center;
+.section {
+  align-items: center;
+  background-color: #EC1C24;
+  background-image: url('./assets/img/bg.jpg');
+  background-position: center;
+  background-size: cover;
+  box-sizing: border-box;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  min-height: 90rem;
+  padding: 3rem 3rem 10rem;
+  width: 100%;
 }
 
 .heart {
-  margin: 0 auto;
-  height: 24rem;
-  width: 24rem;
   cursor: pointer;
+  height: 24rem;
+  margin: 0 auto;
+  width: 24rem;
 }
+
 </style>
