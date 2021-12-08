449db269d5b799ad22a995f76a8decda1f6d8114
Ian Purvis <ian@purvisresearch.com>
2021-12-08 16:41:26 -0600

    Add terraform files

---
+++ infra/cloudfront.tf
@@ -0,0 +1,102 @@
+resource "aws_cloudfront_distribution" "land_lights" {
+  default_cache_behavior {
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+      "PUT"
+    ]
+    cached_methods = [
+      "GET",
+      "HEAD",
+      "OPTIONS"
+    ]
+    compress = true
+    forwarded_values {
+      cookies {
+        forward = "none"
+      }
+      headers = [
+        "Access-Control-Request-Headers",
+        "Access-Control-Request-Method",
+        "Origin"
+      ]
+      query_string = true
+    }
+    target_origin_id       = 0
+    viewer_protocol_policy = "redirect-to-https"
+  }
+  default_root_object = "index.html"
+  enabled             = true
+  ordered_cache_behavior {
+    path_pattern = "/ws"
+    allowed_methods = [
+      "DELETE",
+      "GET",
+      "HEAD",
+      "OPTIONS",
+      "PATCH",
+      "POST",
+      "PUT"
+    ]
+    cached_methods = [
+      "GET",
+      "HEAD",
+      "OPTIONS"
+    ]
+    compress = true
+    forwarded_values {
+      cookies {
+        forward = "none"
+      }
+      headers = [
+        "Access-Control-Request-Headers",
+        "Access-Control-Request-Method",
+        "Origin"
+      ]
+      query_string = true
+    }
+    target_origin_id       = 1
+    viewer_protocol_policy = "redirect-to-https"
+  }
+  origin {
+    custom_origin_config {
+      http_port = 3000
+      https_port = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = [
+        "TLSv1.2"
+      ]
+    }
+    domain_name = aws_instance.land_lights.public_dns
+    origin_id   = 0
+  }
+  origin {
+    custom_origin_config {
+      http_port = 3001
+      https_port = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols = [
+        "TLSv1.2"
+      ]
+    }
+    domain_name = aws_instance.land_lights.public_dns
+    origin_id   = 1
+  }
+  price_class = "PriceClass_100"
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "cloudfront-domain" {
+  value = aws_cloudfront_distribution.land_lights.domain_name
+}
