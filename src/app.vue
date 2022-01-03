2f471bbe4fb3b9d88039085deab35152165378ca
precious <chris@barnesy.me>
2022-01-03 10:10:31 -0500

    Updates graphics and links

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,14 @@
 <template>
   <section class="section">
-    <h1>HeartbeatATL</h1>
+    <h1>Heartbeat_ATL</h1>
     <img @click="start" class="heart" src="~/assets/img/heart.png"/>
-    <pre v-if="position">{{ JSON.stringify(position, null, 2) }}</pre>
-    <pre v-if="error">{{ error.message }}</pre>
+    <!-- <pre v-if="position">{{ JSON.stringify(position, null, 2) }}</pre>
+    <pre v-if="error">{{ error.message }}</pre> -->
+
+    <p class="links">
+      <a href="https://goo.gl/maps/9mYazaiXJWCuJVSW6">@Centenial Yards</a>
+      <a href="https://www.instagram.com/heartbeat_atl">#heartbeatatl</a>
+    </p>
   </section>
 </template>
 
@@ -58,6 +63,10 @@ export default {
 
 <style lang="scss">
 
+body {
+  background-color: #cc6f26;
+}
+
 .section {
   align-items: center;
   background-color: #EC1C24;
@@ -68,16 +77,46 @@ export default {
   color: white;
   display: flex;
   flex-direction: column;
-  min-height: 90rem;
-  padding: 3rem 3rem 10rem;
+  min-height: 80rem;
+  padding: 3rem;
   width: 100%;
+  justify-content: center;
+  align-items: center;
+  padding: 3rem;
+
+
+  .links {
+    display: flex;
+
+    a {
+      display: block;
+      margin: 0 1rem;
+      color: white;
+      text-decoration: none;
+    }
+  }
 }
 
 .heart {
   cursor: pointer;
-  height: 24rem;
+  height: 34rem;
+  width: 34rem;
   margin: 0 auto;
-  width: 24rem;
+}
+
+@media (max-width: 800px) {
+  h1 {
+    font-size: 2.4rem;
+  }
+
+  .heart {
+    height: 28rem;
+    width: 28rem;
+  }
+
+  .section {
+    min-height: 60rem;
+  }
 }
 
 </style>
